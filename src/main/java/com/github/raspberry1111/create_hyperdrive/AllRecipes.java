a9e178d102e80249aa0fe484132244d58a8b0262
Raspberry1111 <raspberry1111@duck.com>
2026-06-08 19:36:13 -0400

    Add mysterious conversion for reverse recipe

--- src/main/java/com/github/raspberry1111/create_hyperdrive/AllRecipes.java
+++ src/main/java/com/github/raspberry1111/create_hyperdrive/AllRecipes.java
@@ -6,10 +6,14 @@ import com.simibubi.create.compat.jei.category.MysteriousItemConversionCategory;
 import net.minecraft.world.item.crafting.RecipeHolder;
 
 public class AllRecipes {
-    private static final RecipeHolder<ConversionRecipe> HYPERDRIVE = ConversionRecipe.create(HyperdriveBlockItem.emptyStack(), HyperdriveBlockItem.filledStack());
+    private static final RecipeHolder<ConversionRecipe> HYPERDRIVE = ConversionRecipe.create(HyperdriveBlockItem.emptyStack(), HyperdriveBlockItem.filledStack()); // using empty hyperdrive on shulker
+    private static final RecipeHolder<ConversionRecipe> EMPTY_HYPERDRIVE = ConversionRecipe.create(HyperdriveBlockItem.filledStack(), HyperdriveBlockItem.emptyStack()); // using filled hyperdrive on shulker box
 
     public static void register() {
         MysteriousItemConversionCategory.RECIPES.remove(HYPERDRIVE);
+        MysteriousItemConversionCategory.RECIPES.add(HYPERDRIVE);
+
+        MysteriousItemConversionCategory.RECIPES.remove(EMPTY_HYPERDRIVE);
         MysteriousItemConversionCategory.RECIPES.add(HYPERDRIVE);
     }
 }
